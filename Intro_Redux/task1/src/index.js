4604536e37a9f56760b34073dfb188df1a2a5d38
Olena Ryabenko <olenaryabenko@gmail.com>
2022-12-30 11:59:57 +0200

    export added

--- Intro_Redux/task1/src/index.js
+++ Intro_Redux/task1/src/index.js
@@ -3,19 +3,19 @@ import { createStore } from 'redux';
 const INCREMENT = 'COUNTER/INCREMENT';
 const DECREMENT = 'COUNTER/DECREMENT';
 
-const incremenFunc = () => {
+export const incremenFunc = () => {
 	return {
 		type: INCREMENT,
 	};
 };
 
-const decrementFunc = () => {
+export const decrementFunc = () => {
 	return {
 		type: DECREMENT,
 	};
 };
 
-const counterReducer = (state = 0, action) => {
+export const counterReducer = (state = 0, action) => {
 	switch (action.type) {
 		case INCREMENT:
 			return state + 1;
@@ -26,4 +26,4 @@ const counterReducer = (state = 0, action) => {
 	}
 };
 
-const store = createStore(counterReducer);
+export const store = createStore(counterReducer);
