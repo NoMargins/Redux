608f30db960362a07728d63da085d69b1e8a852b
Olena Ryabenko <olenaryabenko@gmail.com>
2023-01-16 15:05:13 +0200

    initial commit

---
+++ Async_actions__Midleware/task1/src/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux';
+import { counterReducer } from './counter.reducer';
+
+const logger = (state) => (next) => (action) => {
+	console.group(action.type);
+	console.info('dispatching', action);
+	let result = next(action);
+	console.log('next state', store.getState());
+	console.groupEnd();
+};
+
+const store = createStore(counterReducer, applyMiddleware(logger));
+
+export default store;
